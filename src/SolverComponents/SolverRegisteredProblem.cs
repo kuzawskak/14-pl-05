543f4430276596c6beab958d5d98f07bed9b2bd1
kuzawskak <kasia.kuzawska@gmail.com>
2014-03-23 16:28:03 +0100

    CC ui extended, problem dividing and merging bug removed, new heleper class added for problem count in TM

---
+++ src/SolverComponents/SolverRegisteredProblem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SolverComponents;
+using CommunicationXML;
+
+
+namespace SolverComponents
+{
+
+
+
+    class SolverRegisteredProblem
+    {
+
+        private ulong problem_id;
+        private List<PartialProblem> ongoing_problems = new List<PartialProblem>();
+
+
+        public ulong ProblemId
+        {
+            get { return problem_id; }
+            set { problem_id = value; }
+        }
+
+        public SolverRegisteredProblem(ulong problem_id, List<PartialProblem> ongoing_problems)
+        {
+            this.problem_id = problem_id;
+            this.ongoing_problems = ongoing_problems;
+        }
+
+        public void MarkAsSolved(ulong task_id)
+        {
+            PartialProblem pp = ongoing_problems.Find(x=>x.TaskId ==task_id);
+            if (pp != null)
+                ongoing_problems.Remove(pp);
+        }
+
+        public bool IsProblemSolved()
+        {
+            return ongoing_problems.Count() == 0;
+        }
+
+        
+    }
+}
